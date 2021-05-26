8dd482a15dcbbe4005c1838e8dbe370bd3be00df
Maria DelSignore <maria.ad27@gmail.com>
2021-05-26 15:58:08 -0600

    Render message to page

--- main.js
+++ main.js
@@ -4,20 +4,45 @@
 // *task1 >> when a user selects a msg option and clicks the 'receive msg' btn, a random message from that category will display
 
 // create QS for affirmation, mantra, getMessageBtn
+var chooseAffirmation = document.querySelector('#affirmation');
+var chooseMantra = document.querySelector('#mantra');
+var getMessageBtn = document.querySelector('#getMessageBtn');
+var showMessage = document.querySelector('#statementHere');
+var meditateIcon = document.querySelector('#meditateIcon');
+var messageBox = document.querySelector('#messageBox');
+
 // add event listener for getMessageBtn to listen for click
+getMessageBtn.addEventListener('click', renderMessage);
 // create function chooseMsgType() >> responds to click
   // deciphers which btn has been checked
     // if affirmation, then
     // if mantra, then
+function renderMessage() {
+  var affirmationOption = affirmations[getRandomIndex(affirmations)];
+  var mantraOption = mantras[getRandomIndex(mantras)];
 
+  messageBox.innerHTML = '';
+  if (chooseAffirmation.checked) {
+    messageBox.innerHTML = affirmationOption;
+    console.log('affirmation', affirmationOption);
+  } else if (chooseMantra.checked) {
+    messageBox.innerHTML = mantraOption;
+    console.log('mantra', mantraOption);
+  }
+}
 
 // create function displayMsg()
   // dispalys whichever msg type has been checked by user
   // use .innerHTML or .innerText to adapt var to user choice
   // invoke function in chooseMsgType
+// function renderMessage() {
+//
+// }
 
 // build a getRandomIndex() using Math.floor() to cycle through affirmations and mantras arrays
-
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 
 
