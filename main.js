2c59c05822f7e7c86389954071bba187402ca5cc
Maria DelSignore <maria.ad27@gmail.com>
2021-05-26 16:07:53 -0600

    Remove pseudocode for random affirmation&mantra feature

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 // ** think data model vs DOM **
 // ** remember SRP **
 
-// *task1 >> when a user selects a msg option and clicks the 'receive msg' btn, a random message from that category will display
-
-// create QS for affirmation, mantra, getMessageBtn
 var chooseAffirmation = document.querySelector('#affirmation');
 var chooseMantra = document.querySelector('#mantra');
-var getMessageBtn = document.querySelector('#getMessageBtn');
-var showMessage = document.querySelector('#statementHere');
 var meditateIcon = document.querySelector('#meditateIcon');
+var showMessage = document.querySelector('#statementHere');
 var messageBox = document.querySelector('#messageBox');
+var getMessageBtn = document.querySelector('#getMessageBtn');
 
-// add event listener for getMessageBtn to listen for click
 getMessageBtn.addEventListener('click', renderMessage);
-// create function chooseMsgType() >> responds to click
-  // deciphers which btn has been checked
-    // if affirmation, then
-    // if mantra, then
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 function renderMessage() {
   var affirmationOption = affirmations[getRandomIndex(affirmations)];
   var mantraOption = mantras[getRandomIndex(mantras)];
@@ -24,30 +21,7 @@ function renderMessage() {
   messageBox.innerHTML = '';
   if (chooseAffirmation.checked) {
     messageBox.innerHTML = affirmationOption;
-    console.log('affirmation', affirmationOption);
   } else if (chooseMantra.checked) {
     messageBox.innerHTML = mantraOption;
-    console.log('mantra', mantraOption);
   }
 }
-
-// create function displayMsg()
-  // dispalys whichever msg type has been checked by user
-  // use .innerHTML or .innerText to adapt var to user choice
-  // invoke function in chooseMsgType
-// function renderMessage() {
-//
-// }
-
-// build a getRandomIndex() using Math.floor() to cycle through affirmations and mantras arrays
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-
-
-// *task2 >> when the msg appears, the meditation icon disappears from the msg area
-  // make QS for meditateIcon
-  // make QS for statementHere
-
-// create show() and hide() functions?
