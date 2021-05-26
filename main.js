4e0659b97725bc89f9a1246f2456e01394efe320
Maria DelSignore <maria.ad27@gmail.com>
2021-05-25 23:23:34 -0600

    Think-through pseudocode

---
+++ main.js
@@ -0,0 +1,28 @@
+// ** think data model vs DOM **
+// ** remember SRP **
+
+// *task1 >> when a user selects a msg option and clicks the 'receive msg' btn, a random message from that category will display
+
+// create QS for affirmation, mantra, getMessageBtn
+// add event listener for getMessageBtn to listen for click
+// create function chooseMsgType() >> responds to click
+  // deciphers which btn has been checked
+    // if affirmation, then
+    // if mantra, then
+
+
+// create function displayMsg()
+  // dispalys whichever msg type has been checked by user
+  // use .innerHTML or .innerText to adapt var to user choice
+  // invoke function in chooseMsgType
+
+// build a getRandomIndex() using Math.floor() to cycle through affirmations and mantras arrays
+
+
+
+
+// *task2 >> when the msg appears, the meditation icon disappears from the msg area
+  // make QS for meditateIcon
+  // make QS for statementHere
+
+// create show() and hide() functions?
