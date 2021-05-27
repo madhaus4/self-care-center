d5da233e16a7d9ed1278c1dfcdb399112369f2c1
Maria DelSignore <maria.ad27@gmail.com>
2021-05-27 16:57:10 -0600

    Add prevent function to disable btn click

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ var getMessageBtn = document.querySelector('#getMessageBtn');
 
 var mainPage = document.querySelector('#mainPage');
 var loginPage = document.querySelector('#loginPage');
-var userInput = document.querySelector('#userName');
+var userName = document.querySelector('#userName');
 var letsGoBtn = document.querySelector('#letsGoBtn');
+var userGreeting = document.querySelector('#userGreeting');
 
-// window.addEventListener('load', renderLoginPage);
 getMessageBtn.addEventListener('click', renderMessage);
 letsGoBtn.addEventListener('click', renderMainPage);
 
@@ -35,15 +35,23 @@ function renderMessage() {
 }
 
 function renderMainPage() {
+  prevent();
   loginPage.classList.add('hidden');
   mainPage.classList.remove('hidden');
+  userGreeting.innerText = `Hello ${userName.value}!  Hope you're having a great day!`;
+}
+
+function prevent(event) {
+  if (!userName.value) {
+    event.preventDefault();
+  }
 }
 
 // LOGIN PAGE
-
 // task1 >> refactor application so user lands on a 'login' page
 // task2 >> the login page needs to match style of application
 // task3 >> the login page should contain an input for a user to enter their name and a submit button
+
 // task4 >> after the user clicks the submit button, they are taken to the main page of the application where they will see a personalized greeting that displays their name and a welcome message
 // task5 >> the welcome message & name should appear in a logical place
 
